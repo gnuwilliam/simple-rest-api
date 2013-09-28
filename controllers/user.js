a94d4b4838ce683e0237cbb226f4a8d3b1754a30
William Oliveira <sudowilliam@gmail.com>
2013-09-28 15:04:48 -0300

    add create method

--- controllers/user.js
+++ controllers/user.js
@@ -1,2 +1,20 @@
 var User = require('../models/user.js');
 
+exports.create = function(req, res) {
+  User.findOne({ username: req.body.username }, function(err, user) {
+    if(user != null) {
+      res.send('user exists');
+      return false;
+    }
+
+    new User({
+      name: req.body.fullName,
+      surname: req.body.surname,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    }).save();
+
+    res.send('success');
+  });
+}
